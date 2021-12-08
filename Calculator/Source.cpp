2fe4fc48b8ec0704c8cd47b51344a4a538cc9962
ОрловаА <@218-13>
2021-12-08 21:13:52 +0600

    Oyasumi changes Max's cout :)

--- Calculator/Source.cpp
+++ Calculator/Source.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 int main() {
+	cout << "Я хочу апельсиновый сок" << endl;//Oyasumi
 	cout << "Hello" << endl;//Maks
+	cout << "C++ rules" << endl;
+	for (int i = 0; i < 10; i++) {  //тут был Леха
+		cout << "HELLO WORLD!" << endl;
+	}
 }
